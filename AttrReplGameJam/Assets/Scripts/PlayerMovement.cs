fd77bb58d39182607ab6355b75cfd858f17428e4
Adrian Piwin <adrian7048@gmail.com>
2021-11-12 10:34:57 -0500

    Player Movement and Screen Wrap
    - Player can now move with nice gravity feeling
    - Player faces mouse
    - Player wraps screen when going off screen

---
+++ AttrReplGameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Settings")]
+    public float rotateSpeed = 5f; // Speed to look at mouse
+    public float moveSpeed = 5f; // Speed to reach max vel
+    public float maxVel; // Max achievable velocity
+    public float dragThreshold; // Threshold of vel speed to enable drag
+    public float drag; // Counter movement
+
+    // Private
+    private Rigidbody2D body;
+    private Vector2 playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+        playerInput.y = Input.GetAxisRaw("Vertical");
+
+        rotatePlayer();
+    }
+
+    private void FixedUpdate() {
+        movePlayer();
+    }
+
+    // Move player
+    private void movePlayer(){
+        // Movement
+        if (body.velocity.magnitude < maxVel)
+            body.AddForce(playerInput * moveSpeed);
+
+        // Drag
+        if ((body.velocity.magnitude > dragThreshold && playerInput.magnitude == 0) || body.velocity.magnitude >= maxVel)
+        {
+            body.velocity = new Vector2(body.velocity.x * drag, body.velocity.y * drag);
+        }
+
+        Debug.Log(body.velocity.magnitude);
+    }
+
+    /* Alt movement
+    private void movePlayer()
+    {
+        // Movement
+        body.AddForce(playerInput * moveSpeed);
+
+        // Drag
+        if (body.velocity.magnitude > dragThreshold)
+        {
+            body.velocity = new Vector2(body.velocity.x * drag, body.velocity.y * drag);
+        }
+
+        Debug.Log(body.velocity.magnitude);
+    }*/
+
+    // Rotate player towards mouse
+    private void rotatePlayer(){
+    	// Look at mouse
+	    Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = new Vector2(worldPoint.x - transform.position.x, worldPoint.y - transform.position.y);
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
+    }
+}
---
+++ AttrReplGameJam/Assets/Scripts/ScreenWrap.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrap : MonoBehaviour
+{
+	public float buffer;
+	public float wrapDelay = 0f;
+	public bool canWrap = true;
+
+	private Camera cam;
+	private float distanceZ, leftConstraint, rightConstraint, bottomConstraint, topConstraint;
+
+     void Start () {
+         cam = Camera.main;
+         distanceZ = Mathf.Abs (cam.transform.position.z + transform.position.z);
+ 
+         leftConstraint = cam.ScreenToWorldPoint (new Vector3 (0.0f, 0.0f, distanceZ)).x;
+         rightConstraint = cam.ScreenToWorldPoint (new Vector3 (Screen.width, 0.0f, distanceZ)).x;
+         bottomConstraint = cam.ScreenToWorldPoint (new Vector3 (0.0f, 0.0f, distanceZ)).y;
+         topConstraint = cam.ScreenToWorldPoint (new Vector3 (0.0f, Screen.height, distanceZ)).y;
+	 }
+	 
+	 void FixedUpdate (){
+		// Left constriant
+		if (transform.position.x < leftConstraint - buffer) {
+			StartCoroutine(wrapObject(new Vector3 (rightConstraint + buffer, transform.position.y, transform.position.z)));
+		}
+		// Right constriant
+		else if (transform.position.x > rightConstraint + buffer) {
+			StartCoroutine(wrapObject(new Vector3 (leftConstraint - buffer, transform.position.y, transform.position.z)));
+		}
+		// Bottom constriant
+		else if (transform.position.y < bottomConstraint - buffer) {
+			StartCoroutine(wrapObject(new Vector3 (transform.position.x, topConstraint + buffer, transform.position.z)));
+		}
+		// Top constriant
+		else if (transform.position.y > topConstraint + buffer) {
+			StartCoroutine(wrapObject(new Vector3 (transform.position.x, bottomConstraint - buffer, transform.position.z)));
+		}
+	 }
+
+	 IEnumerator wrapObject(Vector3 newPos){
+	 	yield return new WaitForSeconds(wrapDelay);
+
+	 	if (canWrap){
+	 		transform.position = newPos;
+	 	}
+	 }
+}
